4893c7ccf312801fecc883d47c46087f876f234f
Nexidict <nexidict.developer@gmail.com>
2025-04-13 21:34:43 +0200

    Changed Adipose overall structure

--- ExampleModel/script.lua
+++ ExampleModel/script.lua
@@ -23,9 +23,7 @@ action_wheel:setPage(mainPage)
 local weightTrigger = mainPage:newAction()
 :title("Cycle Weight")
 :item("minecraft:cake")
-:onScroll(function(dir) NudgeWeight(dir) end)
-
-
+--:onScroll(function(dir) NudgeWeight(dir) end)
 
 local stage0 = {
 	models.model.BodyW0,
@@ -35,33 +33,23 @@ local stage0 = {
 	models.model.LeftLegW0,
 	models.model.RightLegW0
 }
-adipose.weightStage:new(
-	stage0, --modelParts
-	animations.model.W0, --Granular anim
-	nil, --Head offset
-	nil, --Hitbox Width
-	nil, --Hitbox Height
-	nil
-)
+adipose.weightStage:newStage()
+	:setParts(stage0)
+	:setGranularAnimation(animations.model.W0)
 
-stage1 = {
+local stage1 = {
 	models.model.BodyW1,
 	models.model.TailW1,
 	models.model.LeftArmW0,
 	models.model.RightArmW0,
 	models.model.LeftLegW1,
 	models.model.RightLegW1
-	}
-adipose.weightStage:new(
-	stage1, --modelParts
-	animations.model.W1, --Granular anim
-	nil, --Head offset
-	nil, --Hitbox Width
-	nil, --Hitbox Height
-	nil
-)
+}
+adipose.weightStage:newStage()
+	:setParts(stage1)
+	:setGranularAnimation(animations.model.W1)
 
-stage2 = {
+local stage2 = {
 	models.model.BodyW2,
 	models.model.TailW1,
 	models.model.LeftArmW2,
@@ -69,16 +57,11 @@ stage2 = {
 	models.model.LeftLegW2,
 	models.model.RightLegW2
 	}
-adipose.weightStage:new(
-	stage2, --modelParts
-	animations.model.W2, --Granular anim
-	nil, --Head offset
-	nil, --Hitbox Width
-	nil, --Hitbox Height
-	nil
-)
+adipose.weightStage:newStage()
+	:setParts(stage2)
+	:setGranularAnimation(animations.model.W2)
 
-stage3 = {
+local stage3 = {
 	models.model.HeadW3,
 	models.model.BodyW3,
 	models.model.TailW3,
@@ -86,17 +69,12 @@ stage3 = {
 	models.model.RightArmW3,
 	models.model.LeftLegW3,
 	models.model.RightLegW3
-	}
-adipose.weightStage:new(
-	stage3, --modelParts
-	animations.model.W3, --Granular anim
-	nil, --Head offset
-	nil, --Hitbox Width
-	nil, --Hitbox Height
-	nil
-)
+}
+adipose.weightStage:newStage()
+	:setParts(stage3)
+	:setGranularAnimation(animations.model.W3)
 
-stage4 = {
+local stage4 = {
 	models.model.HeadW3,
 	models.model.BodyW4,
 	models.model.TailW4,
@@ -104,12 +82,6 @@ stage4 = {
 	models.model.RightArmW4,
 	models.model.LeftLegW4,
 	models.model.RightLegW4
-	}
-adipose.weightStage:new(
-	stage4, --modelParts
-	nil, --Granular anim
-	nil, --Head offset
-	nil, --Hitbox Width
-	nil, --Hitbox Height
-	nil
-)
+}
+adipose.weightStage:newStage()
+	:setParts(stage4)
